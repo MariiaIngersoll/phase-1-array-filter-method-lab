3bce769a3ef0d243ac80e70cf39f67fd9e786e1f
+ <+>
2023-03-01 22:03:38 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, name){
+    const newDrivers = drivers.filter(function(drivers){
+        return drivers.toLowerCase() === name.toLowerCase()});
+    return newDrivers;
+    
+    }
+
+
+    function fuzzyMatch (driverArray,driverName){
+        return driverArray.filter(element =>
+        element.slice(0,1) === driverName.slice(0,1))
+        }
+
+        function matchName(drivers, string ) {
+            return drivers.filter(element => element.name === string)
+        }
